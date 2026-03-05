f68ebf9ae82a99de025aef4310cfe6e83b5f09d2
vimal2512 <ervimalraj96@gmail.com>
2026-03-05 09:11:27 +0530

    chore: install react router react-query axios and setup tailwind v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 function App() {
-  return <h1>Analytics Dashboard</h1>
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">
+        SaaS Analytics Dashboard
+      </h1>
+    </div>
+  );
 }
 
 export default App;
-
