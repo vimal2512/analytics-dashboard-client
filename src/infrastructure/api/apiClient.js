142dc0ff0c39b727f9447fb662fd90c5e349c083
vimal2512 <ervimalraj96@gmail.com>
2026-03-14 11:08:38 +0530

    connect frontend to the production backend

--- src/infrastructure/api/apiClient.js
+++ src/infrastructure/api/apiClient.js
@@ -1,7 +1,30 @@
+// import axios from "axios";
+
+// const apiClient = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   withCredentials: true
+// });
+
+// // Attach token automatically
+// apiClient.interceptors.request.use((config) => {
+
+//   const user = localStorage.getItem("user");
+
+//   if (user) {
+//     const token = JSON.parse(user).token;
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+
+// });
+
+// export default apiClient;
+
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 });
 
